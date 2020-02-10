40fe5f969be93c125a60d7571a7f1a8c299aca21
Sarah Ferguson <sferguson670@gmail.com>
2020-02-09 17:14:42 -0700

    added outputs for system time
    doesn't look the best but it's for data recording

--- hw1/src/Sorts.java
+++ hw1/src/Sorts.java
@@ -6,7 +6,7 @@ import java.util.Random;
 public class Sorts {
     private static int[] originalArray;
 
-    private static int ARRAY_SIZE = 100;
+    private static int ARRAY_SIZE = 1000;
     private static int LARGE_REGION = 11;
 
     private static String originalArrayFile = "original-unsorted-array.txt";
@@ -62,23 +62,38 @@ public class Sorts {
         QuickSort ob = new QuickSort();
 
         int[] quicksortArray = copyArray(originalArray);
+        Long start = getSystemTime();
         ob.quickSort(quicksortArray, 0, quicksortArray.length-1);
+        Long end = getSystemTime();
+        System.out.println("time taken: " + timeTaken(start, end) + " nanoseconds");
         printArrayToFile(quicksortArray, quickSortArrayFile);
 
         int[] medianArray = copyArray(originalArray);
+        Long start2 = getSystemTime();
         ob.medianQuickSort(medianArray, 0, medianArray.length-1);
+        Long end2 = getSystemTime();
+        System.out.println("time taken: " + timeTaken(start2, end2) + " nanoseconds");
         printArrayToFile(medianArray, medianQuickSortedArrayFile);
 
         int[] quicksortInsertionArray = copyArray(originalArray);
+        Long start3 = getSystemTime();
         ob.quickSortAndInsertionSort(quicksortInsertionArray, LARGE_REGION);
+        Long end3 = getSystemTime();
+        System.out.println("time taken: " + timeTaken(start3, end3) + " nanoseconds");
         printArrayToFile(quicksortInsertionArray, quickSortAndInsertionArrayFile);
 
         int[] lomutoArray = copyArray(originalArray);
+        Long start4 = getSystemTime();
         ob.quickSortWithLomutos(lomutoArray, 0, lomutoArray.length-1);
+        Long end4 = getSystemTime();
+        System.out.println("time taken: " + timeTaken(start4, end4) + " nanoseconds");
         printArrayToFile(lomutoArray, quickSortLomutosPartitionArrayFile);
 
         int[] hoareArray = copyArray(originalArray);
+        Long start5 = getSystemTime();
         ob.quickSortWithHoares(hoareArray, 0, hoareArray.length-1);
+        Long end5 = getSystemTime();
+        System.out.println("time taken: " + timeTaken(start5, end5) + " nanoseconds");
         printArrayToFile(hoareArray, quickSortHoarePartitionArrayFile);
     }
 }
