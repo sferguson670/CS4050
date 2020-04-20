c8c6ac10500fb6a2f44b9f6a497e5fae1c3e5151
Sarah Ferguson <sferguson670@gmail.com>
2020-04-19 22:13:14 -0600

    base of prime numbers done
    base hw done, might add methods to randomize each composite number case

--- hw3/src/PrimeNumbers.java
+++ hw3/src/PrimeNumbers.java
@@ -6,5 +6,65 @@
  * See if results improve for larger values of k
  */
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class PrimeNumbers {
+    private static int p;
+    private static int k = 10000;
+    int counter = 0;
+
+    /*
+     * Prompts user to enter a value to be considered the P value
+     */
+    private static void getNumberFromConsole() {
+        System.out.println("Enter an int to be considered P value:");
+        Scanner input = new Scanner(System.in);
+        p = input.nextInt();
+    }
+
+    /*
+     * Generate a random number n where 1 < n < P, k number of times,
+     * each time do check if % will return it as a Prime number,
+     * for each time it is REJECTED (NOT a prime number), add to counter
+     */
+    private void checkRandomValues(int p) {
+        for (int i = 0; i < k; i++) {
+            Random num = new Random();
+            int randomNum = num.nextInt(p);
+            System.out.println("Checking P % " + randomNum);
+            if (!checkIfPrime(p, randomNum))
+                counter++;
+        }
+    }
+
+    /*
+     * Checks if P is prime by doing P % n, where n is random value 1 < n < P
+     * Returns true if "prime", returns false if "not prime" by calculation
+     */
+    private boolean checkIfPrime(int p, int n) {
+        return (p % n != 0);
+    }
+
+    public static void main(String[] args) {
+        PrimeNumbers runner = new PrimeNumbers();
+
+        //getNumberFromConsole();
+        getNumberFromConsole();
+        runner.checkRandomValues(p);
+
+        System.out.println("***********************************************");
+        System.out.println("P value, checking if this is prime: " + p);
+        System.out.println("K value, number of random values to test against: " + k);
+        System.out.println("Total number of rejections: " + runner.counter);
+    }
 }
+
+/*
+ * References used:
+ * https://www.tutorialspoint.com/read-integers-from-console-in-java
+ * https://en.wikipedia.org/wiki/Prime_number
+ * https://www.mathsisfun.com/prime_numbers.html
+ * https://primes.utm.edu/curios/includes/primetest.php
+ * https://mkyong.com/java/java-generate-random-integers-in-a-range/
+ */
