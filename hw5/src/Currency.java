33005900d8bcb10696bee06937ce8a68f84bc55e
Sarah Ferguson <sferguson670@gmail.com>
2020-05-05 15:52:16 -0600

    reading input
    need to work on excluding header

--- hw5/src/Currency.java
+++ hw5/src/Currency.java
@@ -6,10 +6,50 @@
  * 2. find sequences of trades that will results in a profit
  */
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Currency {
+    private static String INPUT_FILE = "exchangeRatesF2016.txt";
+
+    private int[] fileLines;
     private int numOfCurrencies;
 
-    public static void main (String[] args) {
+    private void readFile() {
+        try {
+            Scanner in = new Scanner(new FileInputStream(INPUT_FILE));
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
 
+                if (line.length() > 0 && line.charAt(0) == '*')
+                    continue;
+
+                System.out.println(line);
+                /*
+                String data[] = line.split(",");
+                String fromCountry = data[0];
+                String toCountry = data[1];
+                double currencyRate = Double.parseDouble(data[2]);
+                 */
+            }
+            in.close();
+        }
+        catch (FileNotFoundException e) {
+            //ignoring ...
+        }
+    }
+
+    private void buildWeightedGraph() {
+
+    }
+
+    private void getTradeSequences() {
+
+    }
+
+    public static void main (String[] args) {
+        Currency runner = new Currency();
+        runner.readFile();
     }
 }
